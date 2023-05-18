46f576445ddab9dd4b5fbedc3f9c9b6b3ed08f73
James <soapman@zoho.com>
2023-05-17 23:14:58 -0500

    Wed 05/17/23 23:14 -- Leopard \ jsoper

---
+++ mozilla/firefox/user.js
@@ -0,0 +1,5 @@
+# user.js ( firefox  profile folder )
+
+user_pref("browser.tabs.insertAfterCurrent", false);
+user_pref("browser.tabs.insertRelatedAfterCurrent", false);
+
---
+++ mozilla/thunderbird/user.js
@@ -0,0 +1,4 @@
+# user.js ( thunderbird  profile folder )
+
+user_pref("mail.identity.default.suppress_signature_separator", true);
+
