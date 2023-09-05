727f554ff1d3edae8ae7920ca8c3b187242e88f6
James <soapman@zoho.com>
2023-09-05 15:02:53 -0500

    Tue 09/05/2023 15:02:53.67 -- LEOPARD2

--- mozilla/thunderbird/user.js
+++ mozilla/thunderbird/user.js
@@ -1,4 +1,13 @@
 # user.js ( thunderbird  profile folder )
+#  Settings->Config Editor
+
+user_pref("browser.theme.content-theme", 0);
+user_pref("browser.theme.toolbar-theme", 0);
+user_pref("extensions.activeThemeID", "thunderbird-compact-dark@mozilla.org");
+
+
+user_pref("font.size.variable.x-western", 20);
+user_pref("layout.css.devPixelsPerPx", "1.15");
+user_pref("mail.folderpane.showColumns", true);
 
 user_pref("mail.identity.default.suppress_signature_separator", true);
-
